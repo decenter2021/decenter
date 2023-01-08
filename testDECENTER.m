51eccdd3317b143dc68149665f7b28ceaa2d893c
Leonardo Pedroso <leonardo.pedroso@tecnico.ulisboa.pt>
2023-01-08 13:16:08 +0100

    Update test installation

--- testDECENTER.m
+++ testDECENTER.m
@@ -1,5 +1,5 @@
 function testDECENTER()
-    version = '1.1.1';
+    version = '1.2.0';
     fprintf('----------------------------------------------------------------------------------\n');
     fprintf('Testing installation of DECENTER v%s.\n',version);
     currentDir = pwd;
@@ -9,24 +9,39 @@ function testDECENTER()
         % Check if all the functions are operational
         fprintf('kalmanCentralizedLTI: ');
         kalmanCentralizedLTI(1,1,1,1);
+        fprintf('\t\tOK\n');
+
         fprintf('kalmanOneStepLTI: ');
         kalmanOneStepLTI(1,1,1,1,1);
+        fprintf('\t\tOK\n');
+
         fprintf('kalmanFiniteHorizonLTI: ');
-        kalmanFiniteHorizonLTI(1,1,1,1,1);
+        opts = struct('W',10);
+        kalmanFiniteHorizonLTI(1,1,1,1,1,opts);
+        fprintf('\tOK\n');
+
         fprintf('LQRCentralizedLTI: ');
         LQRCentralizedLTI(1,1,1,1);
+        fprintf('\t\tOK\n');
+
         fprintf('LQROneStepLTI: ');
         LQROneStepLTI(1,1,1,1,1);
+        fprintf('\t\t\tOK\n');
+
         fprintf('LQRFiniteHorizonLTI: ');
-        LQRFiniteHorizonLTI(1,1,1,1,1);
+        opts = struct('W',10);
+        LQRFiniteHorizonLTI(1,1,1,1,1,opts);
+        fprintf('\t\tOK\n');
+
         fprintf('----------------------------------------------------------------------------------\n');
         fprintf('DECENTER v%s was successfully installed.\n',version);
     catch me
+        fprintf('Failed\n');
         fprintf('Installation of DECENTER v%s failed.\n',version);
         switch me.identifier
             case 'MATLAB:UndefinedFunction'
-                fprintf('ERROR: DECENTER-toolbox-%s is not on the MATLAB path.\n',version);
-                fprintf('Sugested action: add DECENTER-toolbox-%s to your MATLAB path on ´Set Path´under\nthe MATLAB ´Home´tab.\n',version);
+                fprintf('ERROR: decenter-%s is not on the MATLAB path.\n',version);
+                fprintf('Sugested action: add decenter-%s to your MATLAB path on Set Path under the\nMATLAB Home tab.\n',version);
             otherwise
                 fprintf('ERROR: An unknown error has occured.\n');
                 fprintf('Sugested action: reinstall DECENTER.\n');
